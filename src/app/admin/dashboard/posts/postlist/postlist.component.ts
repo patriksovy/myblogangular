929572a058076ac67f89fcabe26329ab89f987bb
patriksovy <patrikincel@gmail.com>
2022-12-20 18:56:13 +0100

    pre-push

---
+++ src/app/admin/dashboard/posts/postlist/postlist.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { post } from './post.model';
+import { PostsService } from './posts.service';
+import {Subscription} from 'rxjs';
+
+@Component({
+  selector: 'app-postlist',
+  templateUrl: './postlist.component.html',
+  styleUrls: ['./postlist.component.scss']
+})
+export class PostlistComponent implements OnInit, OnDestroy {
+  posts:post[] =[]
+  private postSub: Subscription;
+  constructor(public ps:PostsService){}
+
+  ngOnInit(): void {
+    this.posts = this.ps.getAll();
+    this.postSub = this.ps.getUpdateListener().subscribe((posts:post[])=>{
+      this.posts = posts;
+    })
+    
+  }
+  ngOnDestroy() {
+    this.postSub.unsubscribe();
+  }
+}
